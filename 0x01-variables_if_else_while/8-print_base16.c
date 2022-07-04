2af0c3474a693f4b3e42fc015745639dcb1be41b
biscuitspam <107024564+biscuitspam@users.noreply.github.com>
2022-07-04 23:33:05 +0300

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	       	putchar(c);
+
+  for (c = 97; c <= 102; i++)
+    putchar(c);
+  
+  putchar("\n")
+
+        return (0);
+}
