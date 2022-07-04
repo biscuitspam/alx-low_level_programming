0ae43a89a2e9be0c1b7282048a514653589f218c
biscuitspam <brandonmwanzia@gmail.com>
2022-07-04 06:20:00 -0700

    How to check whether the last digit is greater than or less than 6

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,11 +11,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+
+	if (n > 5)
+		printf(" Last digit of %d is and is greter than 5\n", n);
+	else if (n < 6)
+		printf(" Last digit of %d is and is less than 6 and not 0\n", n);
 	else
-		printf("%d is zero\n", n);
+		printf(" Last digit of %d is and is 0\n", n);
 	return (0);
 }
