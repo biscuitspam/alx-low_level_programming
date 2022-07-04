6d47d4f0539052b6af31695036cc4ca4f0d85ead
biscuitspam <brandonmwanzia@gmail.com>
2022-07-04 02:26:57 -0700

    How to check whether a variable is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,16 +10,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
 	return (0);
-        if (n > 0); {
-        result = is  positive 
-        }
-        else {n < 0
-        }
-        result = is negative
-        else {n = 0
-        }
-        result = is zero
-        return result;
-        printf("%s", n);
 }
