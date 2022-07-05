6af12f61d6c2f63a4a9067b4d105bd469b4fd13f
biscuitspam <brandonmwanzia@gmail.com>
2022-07-05 14:14:56 -0700

    How to print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,9 +6,15 @@
  */
 int main(void)
 {
-	char c[8] = '_putchar';
-		putchar(c);
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
