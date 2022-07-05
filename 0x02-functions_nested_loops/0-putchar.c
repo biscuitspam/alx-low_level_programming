71986849844d08900dc5029bd33cbaa032c23bdc
biscuitspam <107024564+biscuitspam@users.noreply.github.com>
2022-07-06 00:58:48 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,8 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char print[] = "_putchar";
+	_putchar(print);
 	_putchar('\n');
 
 	return (0);
