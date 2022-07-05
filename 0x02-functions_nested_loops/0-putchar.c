e08d0a65cbc799586f2b061c82c1f581e5cb0923
biscuitspam <brandonmwanzia@gmail.com>
2022-07-05 13:11:47 -0700

    How to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = _putchar;
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
