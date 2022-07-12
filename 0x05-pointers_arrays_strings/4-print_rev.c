0d6278f0ad55ce4a3ae0d088a9847040442b2a8a
biscuitspam <107024564+biscuitspam@users.noreply.github.com>
2022-07-12 21:45:47 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
 /**
- * _puts - prints a string to stdout
- *
- * @str: string to be printed out
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ * Return: no return.
  */
-
-void _puts(char *str)
+void print_rev(char *s)
 {
-	int i;
+	int count = 0;
 
-	for (i = 0; str[i] != 0; i++)
+	while (count >= 0)
 	{
-		_putchar(str[i]);
+		if (s[count] == '\0')
+			break;
+		count++;
 	}
+
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
 	_putchar('\n');
 }
