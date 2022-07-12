1041d78445e33ffa386ba495f4ea3063b7455a3a
biscuitspam <brandonmwanzia@gmail.com>
2022-07-12 09:45:40 -0700

    Updating value of a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * main - updates value of a pointer to 98
+ */
+
+void reset_to_98(int *n);
+{
+	int n;
+	int *p;
+
+	p = &n
+	n = 42
+
+	printf("Your value of 'n' is : %d\n", n);
+	printf("Your address is : %p\n", p);
+
+	*p = 98;
+
+	printf("Your value of 'n' is now : %d\n", n);
+
+	return (0);
+}
