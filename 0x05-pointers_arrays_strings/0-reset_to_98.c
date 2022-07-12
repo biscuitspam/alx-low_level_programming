8fcd02a33e36a1b198f0caa6601ee5ee5d334d69
biscuitspam <107024564+biscuitspam@users.noreply.github.com>
2022-07-12 21:15:19 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
-/*
- * reset_to_98 - updates value of a pointer to 98
- * @n: integer pointer to a variable to be updated
- *
- * Return: nothing
+/**
+ * reset_to_98 - takes a pointer to a parameter and updates its
+ * values.
+ * @n: Input Integer
+ * Return: no return.
  */
-
-void reset_to_98(int *n);
-
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
