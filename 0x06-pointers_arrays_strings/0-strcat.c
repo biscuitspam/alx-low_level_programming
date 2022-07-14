d3e461ff8379471d2506fbb465cb4cb99d708324
biscuitspam <107024564+biscuitspam@users.noreply.github.com>
2022-07-14 13:05:47 +0300

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strcat - concanate two strings
+ * return: char*
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len, _len, i;
+
+	i=0;
+	len - strlen(src);
+	_len - strlen(dest);
+
+	for (i = 0; i < len; i++)
+	{
+		dest[_len + i] = src[i];
+	}
+	return (dets);
+}        
