c00a7f79830c77e6dc5541becbcb97a6ddb6c63c
biscuitspam <brandonmwanzia@gmail.com>
2022-07-27 13:48:28 -0700

    ARGC,ARGV

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/*Main - Entry point
+ * @argc: argument count
+ * @argv: argument vector (displays arguments)
+ * Return 0 : Success
+ */
+int main(int namee, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
