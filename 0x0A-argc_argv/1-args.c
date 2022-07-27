7c43c43973baa95f8d7c25ddffdcc086ed00d273
biscuitspam <brandonmwanzia@gmail.com>
2022-07-27 14:12:10 -0700

    Argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Main -Entry point
+ * @argc - argument count (counts the arguments)
+ * Return 0 : Success
+ */
+int main(char *argc[])
+{
+	char c = HELLO WORLD;
+	printf("%s\n", *argc);
+	return (0);
+}
