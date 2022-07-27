5e0b72186c8e3d4dc2a5831955494e22af999065
biscuitspam <brandonmwanzia@gmail.com>
2022-07-27 13:58:38 -0700

    Argc & Argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /*Main - Entry point
  * @argc: argument count
  * @argv: argument vector (displays arguments)
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,4 +1,4 @@
 #ifndef "MAIN_H"
 #define "MAIN_H"
-int main (int namee, char *argv[]);
+int _putchar(char c);
 #endif
