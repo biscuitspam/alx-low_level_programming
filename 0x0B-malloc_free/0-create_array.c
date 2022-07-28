a837eaec47ba1ccd128f3be097ccf9e17f5682e2
biscuitspam <brandonmwanzia@gmail.com>
2022-07-28 02:59:23 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *s;
-
+	
 	if (size == 0)
 		return (NULL);
 	s = (char *)malloc(size * sizeof(char));
@@ -18,6 +18,5 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		s[i] = c;
-
 	return (s);
 }
