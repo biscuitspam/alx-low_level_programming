7968bc267210e60131d059b6afa9635d4d54caf0
biscuitspam <107024564+biscuitspam@users.noreply.github.com>
2022-09-08 16:25:23 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 /**
- * add_node - adds a new node at the beginning of a list.
- * of a list_t list
- * @head: the first node.
- * @str: string stored in the list.
- * Return : address of the head.
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
 
-list_t *add_node(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	size_t nchar;
@@ -18,11 +18,12 @@ list_t *add_node(list_t **head, const char *str);
 
 	new->str = strdup(str);
 
-	for (nchar = 0; str [nchar]; nchar++)
+	for (nchar = 0; str[nchar]; nchar++)
 		;
 
 	new->len = nchar;
 	new->next = *head;
 	*head = new;
+
 	return (*head);
 }
